579dbf444ce3c65931dd8736eee6683ad697541d
isandeep4 <39699920+isandeep4@users.noreply.github.com>
2025-08-04 09:49:08 -0600

    fixed deployment error

--- apps/frontend/src/app/dashboard/page.tsx
+++ apps/frontend/src/app/dashboard/page.tsx
@@ -7,5 +7,9 @@ import { useUser } from "../contextApi/userContext";
 export default function DashboardPage() {
   const { user } = useUser();
 
+  if (!user) {
+    return <div>Loading...</div>; // Fallback if user is null initially
+  }
+
   return <PatientsListPage roles={user?.roles ?? []} />;
 }
